adabb9e93a27ff406007a228ffc9bfd92918fbf1
mast3r123 <abhishek.mast3r@gmail.com>
2022-05-26 12:21:46 -0400

    Constant and quadratic time

--- Big O.playground/Contents.swift
+++ Big O.playground/Contents.swift
@@ -14,6 +14,8 @@ func findNemo(array: [String]) {
 
 findNemo(array: arr) //O(n) -> Linear time
 
+//=========//
+
 let boxes = [0, 1, 2, 3, 4, 5]
 
 func logTwoBoxes(array: [Int]) {
@@ -21,4 +23,17 @@ func logTwoBoxes(array: [Int]) {
     print(array[1]) // O(1)
 }
 
-logTwoBoxes(array: boxes)
+logTwoBoxes(array: boxes) //O(1) -> Constant
+
+//=========//
+let combination = [1, 2, 3, 4, 5]
+
+func printCombinations(for input: [Int]) {
+    for i in 0..<input.count {
+        for j in 0..<input.count {
+            print(input[i], input[j])
+        }
+    }
+}
+
+printCombinations(for: combination) //O(n ^ 2) -> Quadratic
