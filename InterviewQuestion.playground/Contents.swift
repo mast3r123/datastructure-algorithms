d83ac3b68c90eb25bd2e9e4a1966d0400ba136af
mast3r123 <abhishek.mast3r@gmail.com>
2022-05-28 11:18:38 -0400

    Sample interview question practice

---
+++ InterviewQuestion.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+// Given 2 arrays, create a function that let's a user know (true/false) whether these two arrays contain any common items
+//For Example:
+//const array1 = ['a', 'b', 'c', 'x'];//const array2 = ['z', 'y', 'i'];
+//should return false.
+//-----------
+//const array1 = ['a', 'b', 'c', 'x'];//const array2 = ['z', 'y', 'x'];
+//should return true.
+
+// 2 parameters - arrays - no size limit
+// return true or false
+
+
