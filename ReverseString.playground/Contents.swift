21baee8f0081f33e8404fec179ba0038e2e32576
mast3r123 <abhishek.mast3r@gmail.com>
2022-06-02 20:56:46 -0400

    More ways to reverse string

--- ReverseString.playground/Contents.swift
+++ ReverseString.playground/Contents.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Foundation
 //Create a function that reverses a string:
 // "Master" should be
 // "retsaM"
@@ -14,4 +15,33 @@ func reverseString(input: String) -> String {
     return arr.joined()
 }
 
-print(reverseString(input: "Master Abhishek"))
+
+
+//print(reverseString(input: "Master Abhishek"))
+
+//O(n)
+func reverseString1(input: String) -> String {
+    if (input.isEmpty || input.count < 2) {
+        return "Invalid string"
+    }
+    
+    let inputArray = Array(input)
+    var backwards = [Character]()
+    let totalItems = input.count - 1
+    
+    for i in stride(from: totalItems, through: 0, by: -1) {
+        backwards.append(inputArray[i])
+    }
+    
+    return String(backwards)
+}
+
+//print(reverseString1(input: "master"))
+
+
+//O(1)
+func reverseString2(input: String) -> String {
+    return String(input.reversed())
+}
+
+print(reverseString2(input: "master"))
