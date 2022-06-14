e135f74b3f74cbfb82abd7f7ca9dc965c258a0e0
mast3r123 <abhishek.mast3r@gmail.com>
2022-06-14 16:09:02 -0400

    Insert at index

--- LinkedList.playground/Contents.swift
+++ LinkedList.playground/Contents.swift
@@ -25,6 +25,7 @@ struct LinkedList<Value> {
     var head: Node<Value>?
     var tail: Node<Value>?
     
+    //O(1)
     mutating func append(value: Value) {
         //List is empty
         if head == nil {
@@ -36,8 +37,8 @@ struct LinkedList<Value> {
         }
     }
     
+    //O(1)
     mutating func prepend(value: Value) {
-        
         if head == nil {
             head = Node.init(value: value)
             tail = head
@@ -45,10 +46,41 @@ struct LinkedList<Value> {
             head = Node.init(value: value, next: head)
         }
     }
+    
+    mutating func insert(index: Int, value: Value) {
+        if index < 0 {
+            print("Invalid index")
+        }
+        
+        if index == 0 {
+            prepend(value: value)
+            return
+        } else {
+            let newNode = Node(value: value)
+            let leader = traverseToIndex(index: index - 1)
+            let holdingPointer = leader?.next
+            leader?.next = newNode
+            newNode.next = holdingPointer
+        }
+    }
+    
+    mutating func traverseToIndex(index: Int) -> Node<Value>? {
+        var counter = 0
+        var currentNode = head
+        
+        while(counter != index) {
+            currentNode = currentNode?.next
+            counter += 1
+        }
+        
+        return currentNode
+    }
 }
 
 var ll = LinkedList<Int>()
 ll.append(value: 9)
 ll.append(value: 11)
 ll.prepend(value: 5)
+print(ll)
+ll.insert(index: 1, value: 10)
 print(ll)
