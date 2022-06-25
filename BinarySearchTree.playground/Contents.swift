48950c8bdc1789784c7c4c06f03f1c23fdd8bc83
mast3r123 <abhishek.mast3r@gmail.com>
2022-06-24 21:33:12 -0400

    Binary search tree with insert

---
+++ BinarySearchTree.playground/Contents.swift
@@ -0,0 +1,85 @@
+import UIKit
+
+class Node<T: Comparable> {
+    var element: T
+    var left: Node<T>?
+    var right: Node<T>?
+    
+    init(element: T) {
+        self.element = element
+    }
+}
+
+extension Node: CustomStringConvertible {
+    public var description: String {
+        var s = ""
+        if let left = left {
+            s += "(\(left.description)) <- "
+        }
+        s += "\(element)"
+        if let right = right {
+            s += " -> (\(right.description))"
+        }
+        return s
+    }
+}
+
+extension BinaryTree:CustomStringConvertible {
+    var description: String {
+        guard let root = root else {
+            return "Empty Tree"
+        }
+        return String(describing:root)
+    }
+    
+}
+
+struct BinaryTree<T: Comparable> {
+    
+    var root: Node<T>?
+    
+    init() {
+        root = nil
+    }
+    
+    mutating func insert(value: T) {
+        let newNode = Node(element: value)
+        if root == nil {
+            root = newNode
+        } else {
+            var currentNode = root
+            while (true) {
+                if value < currentNode!.element {
+                    
+                    if(currentNode?.left == nil) {
+                        currentNode?.left = newNode
+                        break
+                    } else {
+                        currentNode = currentNode?.left
+                    }
+                } else {
+                    if(currentNode?.right == nil) {
+                        currentNode?.right = newNode
+                        break
+                    } else {
+                        currentNode = currentNode?.right
+                    }
+                }
+            }
+        }
+    }
+    
+    mutating func lookup() {
+        
+    }
+}
+
+var tree = BinaryTree<Int>()
+tree.insert(value: 9)
+tree.insert(value: 4)
+tree.insert(value: 6)
+tree.insert(value: 20)
+tree.insert(value: 170)
+tree.insert(value: 15)
+tree.insert(value: 1)
+print(tree)
